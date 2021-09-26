ad158c9dd0811922ea9f9cd8b71d2f5b1e25595f
Sazzad Hossain <sazzad.csebubt@gmail.com>
2021-09-26 19:10:15 +0600

    commit

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Cart = (props) => {
     const { cart } = props || {}
+    const totalBudget = (pre, curr) => pre + curr.Budget
+    const total = cart.reduce(totalBudget, 0)
     return (
         <div>
             <div class="card text-white bg-dark mb-3" style={{ "max-width": "18rem" }}>
@@ -9,6 +11,7 @@ const Cart = (props) => {
                 <div class="card-body">
 
                     <h5><i class="fas fa-folder-plus"></i> {cart.length}</h5>
+                    <h5>Total Budget: ${total} million</h5>
                     <ul>
                         {
                             cart.map(data => <li>{data.Title}</li>)
